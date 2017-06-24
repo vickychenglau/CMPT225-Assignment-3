40529387cbc41d8570d6ca50555e0ee33a476750
vcl2 <vcl2@sfu.ca>
2017-06-23 18:51:12 -0700

    Update Node.h

--- Node.h
+++ Node.h
@@ -1,25 +1,57 @@
-/******
-Node class for out linked List queue
-
-
-******/
-#include "Customers.h"
+/*
+ * Node.h - Header and Implementation of Node class
+ * 
+ * Class Description: A singly linked list Node
+ * Class Invariant:
+ *
+ * Last modified on: June 23, 2017
+ * Author: Jacky Tse, Vicky Lau
+ */
+ 
 #pragma once
 
+#include <cstdio>  // Needed for NULL
+
+using namespace std;
+
+
+template <class ElementType>
 class Node{
-private:
-	int data;
-	Node* next;
 
 public:
+	// Public attributes
+	ElementType data;	// Public because more convienent than getters/setters 
+	Node* next;			// Can't use -> notation with private I think
+
+	// Constructors
 	Node();
-	Node(int thedata);
-	Node(int thedata, Node* theNextNode);
+	Node(ElementType theData);
+	Node(ElementType theData, Node* theNextNode);
 
-	setNodeData(int thedata);
-	setNodeNext(Node* theNextNode);
+}; // end Node header
 
-	getNodeData(int thedata);
-	getNodeNext(Node* theNextNode);
 
-}; //end Node
+// Start of implementation of methods
+
+// Default Constructor
+template <class ElementType>
+Node<ElementType>::Node(){
+	next = NULL;				// No data because type is not known
+}
+
+// Parameterized Constructor with data
+template <class ElementType>
+Node<ElementType>::Node(ElementType theData){
+	data = theData;
+	next = NULL;
+}
+
+// Parameterized Constructor with data and next
+template <class ElementType>
+Node<ElementType>::Node(ElementType theData, Node* theNextNode){
+	data = theData;
+	next = theNextNode;
+}
+// End of Node implementation
+
+// End of Node.h
