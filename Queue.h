e928df12c36c29ec4e5e58895aff666473440908
vcl2 <vcl2@sfu.ca>
2017-06-23 18:52:00 -0700

    Update Queue.h

--- Queue.h
+++ Queue.h
@@ -1,15 +1,31 @@
-#include <node.h>
-/*****
-	Description: A Linked List implimentation of a queue
+/*
+ * Queue.h - Header and Implementation of Queue class
+ * 
+ * Class Description: A Linked List implimentation of a FIFO/LILO queue
+ * Class Invariant:           
+ *
+ * Last modified on: June 23, 2017
+ * Author: Jacky Tse, Vicky Lau
+ */
 
-*****/
+#pragma once
 
+#include <cstdio>  // Needed for NULL
+#include <string>
+#include <iostream>
+#include "Node.h"
+#include "EmptyDataCollectionException.h"
+
+using namespace std; // Needed for cout, cin to be recognized
+
+
+template <class ElementType>
 class Queue {
 
 private:
 	int elementCount;
-	Node* head;
-	Node* tail;
+	Node<ElementType>* head;
+	Node<ElementType>* tail;
 
 // We must complete this section.
 
@@ -18,7 +34,6 @@ public:
 // Let's put our constructor(s) and destructor (if any) ***here***.
 
 	Queue();
-
 
 	~Queue();
 
@@ -40,7 +55,7 @@ public:
 
  ////????!!!!!!!!!!!!!!!!!!!!!!!!!!???????????? REMEMBER TO CHANGE BACK TO ORIGINAL IMPLIMENTATION
    //bool enqueue(const ElementType& newElement);
-   bool enqueue(const int newElement);
+   bool enqueue(const ElementType& newElement);
    
    // Description: Removes the element at the "front" of this Queue 
    //              (not necessarily the "front" of its data structure) and  
@@ -54,10 +69,110 @@ public:
    // Postcondition: This Queue is unchanged.
    // Exceptions: Throws EmptyDataCollectionException if this Queue is empty.
    // Time Efficiency: O(1)
-   ElementType peek() const throw(EmptyDataCollectionException);  
+   ElementType peek() const throw(EmptyDataCollectionException);
+
+   // Prints all the data in queue.
+   void printQueue();
  
 /******* Public Interface - END - *******/
 
 // Let's feel free to add other private helper methods to our Queue class.
       
-}; // end Queue
+}; // end Queue Header
+
+
+// Start of implementation of methods
+
+
+//Default Constructor
+template <class ElementType>
+Queue<ElementType>::Queue(){
+   elementCount = 0;
+   head = NULL;
+   tail = NULL;
+}
+
+// Description: Returns the number of elements in the Queue.
+// (This method eases testing.)
+// Time Efficiency: O(1)
+template <class ElementType>
+int Queue<ElementType>::getElementCount() const{
+   return(elementCount);
+}
+
+// Description: Returns "true" is this Queue is empty, otherwise "false".
+// Time Efficiency: O(1)
+template <class ElementType>
+bool Queue<ElementType>::isEmpty() const{
+   if(getElementCount() == 0){
+      return(true);
+   }
+
+   return(false);
+}
+
+// Description: Adds newElement to the "back" of this Queue 
+//              (not necessarily the "back" of its data structure) and 
+//              returns "true" if successful, otherwise "false".
+// Time Efficiency: O(1)
+template <class ElementType>
+bool Queue<ElementType>::enqueue(const ElementType& newElement){
+   Node<ElementType> *newNode = new Node<ElementType>(newElement);
+   
+   if(head == NULL){    //For the case it is empty
+      head = newNode;
+      tail = newNode;
+   }
+   else{
+      tail->next = newNode;   //Inserting at the end of LL
+      tail = newNode;         //updating tail
+   }
+
+   elementCount++;
+
+   return(true);
+}
+
+// Description: Removes the element at the "front" of this Queue 
+//              (not necessarily the "front" of its data structure) and  
+//              returns "true" if successful, otherwise "false".
+// Precondition: This Queue is not empty.
+// Time Efficiency: O(1)
+template <class ElementType>
+bool Queue<ElementType>::dequeue(){
+   Node<ElementType>* current = head;
+   head = head->next;
+   delete current;
+
+   elementCount--;
+
+   return(true);
+}
+
+// Description: Returns (a copy of) the element located at the "front" of this Queue.
+// Precondition: This Queue is not empty.
+// Postcondition: This Queue is unchanged.
+// Exceptions: Throws EmptyDataCollectionException if this Queue is empty.
+// Time Efficiency: O(1)
+template <class ElementType>
+ElementType Queue<ElementType>::peek() const throw(EmptyDataCollectionException){
+   if (isEmpty()){
+      throw EmptyDataCollectionException("peek() called with empty queue.");
+   }
+
+   return(head->data);
+}
+template <class ElementType>
+void Queue<ElementType>::printQueue(){
+   Node<ElementType>* current = head;
+
+   while(current != NULL){
+      cout << current->data << ", ";
+      current = current->next;
+   }
+}
+
+// End of Queue Implementation
+
+// End of Queue.h
+
