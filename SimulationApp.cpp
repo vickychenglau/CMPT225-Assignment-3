593c7d3646955b2832d59fe3878148f6671122b3
vcl2 <vcl2@sfu.ca>
2017-06-30 16:40:58 -0700

    Update SimulationApp.cpp

--- SimulationApp.cpp
+++ SimulationApp.cpp
@@ -3,8 +3,10 @@
  * 
  * Class Description: Simulates a line up with one teller.
  *					  Calculates the average wait time for each customer.
+ *					  Also outputs the number of customers and the time of each
+ *					  customer's arrival/departure in order.
  *
- * Last modified on: June 24, 2017
+ * Last modified on: June 30, 2017
  * Author: Jacky Tse, Vicky Lau
  */
 
@@ -18,25 +20,22 @@
 
 using namespace std;
 
-
 int main(void){
 	// Initializing variables
-
 	string aLine;
 
-	Queue<Customer> *bankLine = new Queue<Customer>();
+	Customer newEvent;
 
+	Queue<Customer> *bankLine = new Queue<Customer>();
 	PriorityQueue<Customer> *eventPriorityQueue = new PriorityQueue<Customer>();
 
-	Customer *newEvent = NULL;
+	unsigned int time 	  		= 0;
+	unsigned int length   		= 0;
+	unsigned int departureTime  = 0;
+	unsigned int currentTime    = 0;
+	unsigned int numOfCustomers = 0;
 
-	unsigned int time 	  		 = 0;
-	unsigned int length   		 = 0;
-	unsigned int departureTime   = 0;
-	unsigned int currentTime     = 0;
-	unsigned int numOfCustomers  = 0;
-
-	float totalWaitTime = 0.0;
+	float totalWaitTime   = 0.0;
 	float averageWaitTime = 0.0;
 
 	bool tellerAvailable = true;
@@ -47,9 +46,10 @@ int main(void){
 /******************************************************
 	try{
 		cout << "Peeking before enqueuing:" << endl;
-		*newEvent = eventPriorityQueue->peek();
+		newEvent = eventPriorityQueue->peek();
 		((cout << "Now, let's have a look at the peeked Customer:")<< newEvent )<< endl;  
-		}catch(EmptyDataCollectionException&anException){
+	}
+	catch(EmptyDataCollectionException&anException){
 		cout << "peeking() unsuccessful because " << anException.what() << endl;
 		// Place recovery code here!
 		}
@@ -62,9 +62,9 @@ int main(void){
       	ss >> time >> length;
 
       	// Arrival Customer created
-      	newEvent = new Customer("A", time, length);
+      	newEvent = Customer("A", time, length);
 
-      	if (!eventPriorityQueue->enqueue(*newEvent)){
+      	if (!eventPriorityQueue->enqueue(newEvent)){
         	return(1);
       	}
 
@@ -74,72 +74,92 @@ int main(void){
 /**************************************************
     try{
 		cout << "Peeking before Processing events:" << endl;
-		*newEvent = eventPriorityQueue->peek();
+		newEvent = eventPriorityQueue->peek();
 		((cout << "Now, let's have a look at the peeked Customer:")<< newEvent )<< endl;  
-		}catch(EmptyDataCollectionException&anException){
+	}
+	catch(EmptyDataCollectionException&anException){
 		cout << "peeking() unsuccessful because " << anException.what() << endl;
 		// Place recovery code here!
-		}
+	}
 **************************************************/
 
     // Event loop
     while(!eventPriorityQueue->isEmpty()){
     	try{
-      		*newEvent = eventPriorityQueue->peek();
-      	}catch(EmptyDataCollectionException&anException){
+      		newEvent = eventPriorityQueue->peek();
+      	}
+      	catch(EmptyDataCollectionException&anException){
 			cout << "peeking() unsuccessful because " << anException.what() << endl;
 		}
 
       	// Calculates current time
-      	currentTime = newEvent->getTime();
+      	currentTime = newEvent.getTime();
 
       	// Process an Arrival Event
-      	if(newEvent->getType() == "A"){
-      		cout << "Processing an arrival event at time:   " << setw(2) << right << currentTime << endl;
+      	if(newEvent.getType() == "A"){
+      		cout << "Processing an arrival event at time:   " << setw(2) << right 
+      			 << currentTime << endl;
 
-      		eventPriorityQueue->dequeue();
+      		if(!eventPriorityQueue->dequeue()){
+      			return(1);
+      		}
 
+      		// Customer dooesn't line up and goes directly to teller
 			if(bankLine->isEmpty() && tellerAvailable){
-				departureTime = currentTime + newEvent->getLength();
+
+				departureTime = currentTime + newEvent.getLength();
 
 				// Departing Customer created since at the teller
-				newEvent = new Customer("D", departureTime, 0);
+				newEvent = Customer("D", departureTime, 0);
 
 				// Adds a Departure Event to the Priority Queue
-				if(!eventPriorityQueue->enqueue(*newEvent)){
+				if(!eventPriorityQueue->enqueue(newEvent)){
 					return(1);
 				}
 
 				tellerAvailable = false;
 			}
+			// Customer lines up
 			else{
-				if(!bankLine->enqueue(*newEvent)){
+				if(!bankLine->enqueue(newEvent)){
 					return(1);
 				}
 			}
 		}
 		// Process a Departure Event
       	else{
-      		cout << "Processing a departure event at time:  " << setw(2) << right << currentTime << endl;
+      		cout << "Processing a departure event at time:  " << setw(2) << right 
+      			 << currentTime << endl;
 
-      		eventPriorityQueue->dequeue();
+      		// Dequeuing a departure event since customer is leaving
+      		if(!eventPriorityQueue->dequeue()){
+      			return(1);
+      		}
+
+      		// Enqueues the leaving customer into a delete queue
+      		// since deleting here causes a segmentation fault
 
       		if(!bankLine->isEmpty()){
       			// Customer at front of line begins transaction
       			try{
-      				*newEvent = bankLine->peek();
-      			}catch(EmptyDataCollectionException&anException){
+      				newEvent = bankLine->peek();
+      			}
+      			catch(EmptyDataCollectionException&anException){
 					cout << "peeking() unsuccessful because " << anException.what() << endl;
 				}
 
-      			totalWaitTime += (currentTime - newEvent->getTime());
+      			totalWaitTime += (currentTime - newEvent.getTime());
 
-      			bankLine->dequeue();
+      			if(!bankLine->dequeue()){
+      				return(1);
+      			}
 
-      			departureTime = currentTime + newEvent->getLength();
-      			newEvent      = new Customer("D", departureTime, 0);
+      			departureTime = currentTime + newEvent.getLength();
 
-      			if(!eventPriorityQueue->enqueue(*newEvent)){
+				// Next customer's departing event
+      			newEvent = Customer("D", departureTime, 0);
+
+      			if(!eventPriorityQueue->enqueue(newEvent)){
       				return(1);
       			}
       		}
@@ -149,14 +169,18 @@ int main(void){
       	}
     }
 
-    averageWaitTime = totalWaitTime/numOfCustomers;
-
 	cout << "Simulation Ends" << endl << endl;
 
+
+	averageWaitTime = totalWaitTime / numOfCustomers;
+
 	cout << "Final Statistics:" << endl;
-	cout << "	Total number of people processed: " << numOfCustomers << endl;
+	cout << "	Total number of people processed:  " << numOfCustomers << endl;
 	cout << "	Average amount of time spent waiting: " 
 		 << averageWaitTime << endl;
+
+	delete bankLine;
+	delete eventPriorityQueue;
 
 	return(0);
 }
